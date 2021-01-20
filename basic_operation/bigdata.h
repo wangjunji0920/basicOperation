201bbffa81c524ab04623bac7130318db9d15430
junji wang <wangjunji0920@outlook.com>
2021-01-20 16:57:19 +0800

    update

--- basic_operation/bigdata.h
+++ basic_operation/bigdata.h
@@ -3,10 +3,44 @@
 #include<istream>
 #include<ostream>
 #include<iostream>
-#include"basic_types.h"
+#include<string>
 using namespace std;
 class bignum
 {
+public:
+	bignum();
+	bignum(string);
+	friend istream& operator>>(istream& in, bignum&);
+	friend ostream& operator<<(ostream& out, bignum);
+private:
 	bool sign;
-
+	string value;
 };
+istream& operator>>(istream& in, bignum& get)
+{
+	string tmp;
+	unsigned i;
+	cin >> tmp;
+	if (tmp[0] == '-')get.sign = 1;
+	else get.sign = 0;
+	get.value.resize(tmp.size()+1);
+	for (i = 0; i < tmp.size(); i++)
+	{
+
+	}
+	return in;
+}
+ostream& operator<<(ostream& out, bignum get)
+{
+	if (get.sign == 0)cout << get.value;
+	else cout << '-' << get.value;
+	return out;
+}
+bignum::bignum()
+{
+	sign = 0;
+}
+bignum::bignum(string a)
+{
+
+}
